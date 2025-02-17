76e92b7095f2b800a213f314f3bd3f5b60ca9e34
CleytonSM <cleytonsm1309@gmail.com>
2025-02-17 13:41:25 -0300

    feat: add SecretKeyHelper for jwt

---
+++ src/main/java/br/com/arquitec/helpers/SecretKeyHelper.java
@@ -0,0 +1,19 @@
+package br.com.arquitec.helpers;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class SecretKeyHelper {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    public SecretKey secretKeyBuilder() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+}
